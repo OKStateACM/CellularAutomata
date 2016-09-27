9097ad9091d4ecfe5a3648c13fa2c6fca1660cac
Alex <o0o.moro.o0o@gmail.com>
2016-09-27 17:07:33 -0500

    added stubs for functionality and added some javadocs

--- Map.java
+++ Map.java
@@ -18,6 +18,19 @@ public class Map {
 		this( Parser.load(filename) ); //fallthrough to the normal constructor once we load the array
 	}
 
+	/** return the value in the cell given at x,y
+	 * @param x x coordinate of the cell
+	 * @param y y coordinate of the cwll
+	 */
+	public char get(int x, int y){
+	}
+	/** Set a cell at x,y position to a certain value
+	 */
+	public void set(int x, int y, char cell){
+	}
+	/** Save the map to a text file
+	 * @param filename name of the file to save to
+	 */
 	public void save(String filename){
 		FileOutputStream file = null;
 		try {
@@ -35,6 +48,8 @@ public class Map {
 		
 	}
 
+	/*display this map as a 2d grid,
+	 */
 	public String toString(){
 		StringBuilder str = new StringBuilder();
 		for (int yi = 0; yi<this.y; yi++) {
