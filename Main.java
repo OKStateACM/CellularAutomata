952369e90f0d37913d3ce6c8da1f4259055977a7
DURVILLE ALEX G <adurvil@csx7.cs.okstate.edu>
2016-09-27 14:58:14 -0500

    initial implementation of saving to a file. another great way to test if loading works correctly

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		char[][] map = new Parser().load("test.txt");
-		Map mapymap = new Map(map);
+		Map mapymap = new Map("test.txt");
 		System.out.println(mapymap);
+		mapymap.save("savefile.txt");
 	}
 }
--- Map.java
+++ Map.java
@@ -1,8 +1,10 @@
+import java.io.*;
 
 public class Map {
 	char[][] map;
 	int x;
 	int y;
+
 	public Map(char[][] map){
 		if (map == null) {
 			map = new char[1][1]; // make a dummy map, rather than crashing on null pointer exceptions.
@@ -11,9 +13,28 @@ public class Map {
 		this.x = map.length;
 		this.y = map[0].length;
 	}
+
 	public Map(String filename){
-		this( Parser.load(filename) ); //fallthrough to the normal constructor once we have the array
+		this( Parser.load(filename) ); //fallthrough to the normal constructor once we load the array
 	}
+
+	public void save(String filename){
+		FileOutputStream file = null;
+		try {
+			file = new FileOutputStream(filename);
+			file.write( this.toString().getBytes() );
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally {
+			try {
+				if (file != null){ file.close(); }
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
 	public String toString(){
 		StringBuilder str = new StringBuilder();
 		for (int yi = 0; yi<this.y; yi++) {
