bed155af63d05bda055bf5cb4cc177f5de7de7ad
Alex <o0o.moro.o0o@gmail.com>
2016-09-27 17:47:44 -0500

    added stub automata class

---
+++ Automata.java
@@ -0,0 +1,24 @@
+public class Automata {
+	Map map;
+
+	/** Constructor
+	 */
+	public Automata( Map map ){
+		this.map = map;
+	}
+	/** Count the number of live neighbors
+	 * @param x x position in the map of where we are centered
+	 * @param y y position in the map of where we are centered
+	 */
+	private neighbors(int x, int y){
+	}
+	/** Simulate one round of the automata
+	 */
+	public void step(){
+	}
+	/** Print friendly string
+	 */
+	public String toString(){
+		 return this.map.toString();
+	}
+}
