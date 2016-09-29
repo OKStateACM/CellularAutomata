f31927ea35f2a810046589986f422f9a4075eef5
Alex <o0o.moro.o0o@gmail.com>
2016-09-29 13:22:18 -0500

    initial implementation of neighbors() method

--- Automata.java
+++ Automata.java
@@ -11,6 +11,17 @@ public class Automata {
 	 * @param y y position in the map of where we are centered
 	 */
 	private neighbors(int x, int y){
+		int count = 0;
+		for (int xi=x-1; xi<=x+1; xi++){
+			for (int yi=y-1; yi<=y+1; yi++){
+				if (this.map.get(xi, yi) == this.alive){
+					count++;
+				}
+			}
+		}
+		if (this.map.get(x,y) == this.alive){
+			count--;
+		}
 	}
 	/** Simulate one round of the automata
 	 */
